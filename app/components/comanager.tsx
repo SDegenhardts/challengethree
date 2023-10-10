12898d2f09e075545333c1f46470bd7246356988
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-10-10 16:44:08 -0300

    Drachen

--- app/components/comanager.tsx
+++ app/components/comanager.tsx
@@ -3,18 +3,17 @@
 import { styled } from "styled-components"
 import Comstorage from "./comstorage"
 import { useState,useEffect } from "react"
+import {BsFillArrowRightSquareFill} from "react-icons/bs"
+import {BsFillArrowLeftSquareFill} from "react-icons/bs"
 
 export default function Comanager() {
 
-    const setad = "->"
-    const setae = "<-"
-
     return (
         <Wrapper>
-            <main className="flex justify-center items-center text-yellow-300 p-16">
-                <button>{setae}</button>
+            <main className="flex items-center justify-between w-[1500px] h-[300px] text-yellow-300 p-16">
+                <button className="text-4xl hover:scale-125 transition-all"><BsFillArrowLeftSquareFill/></button>
                 <Comstorage/>
-                <button>{setad}</button>
+                <button className="text-4xl hover:scale-125 transition-all"><BsFillArrowRightSquareFill/></button>
             </main>
         </Wrapper>
     )
@@ -23,10 +22,11 @@ export default function Comanager() {
 const Wrapper = styled.image`
 
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     width: 1000px;
     height: 300px;
     background-color: #000000a2;
     margin: 7%;
+    overflow: auto;
 
 `
--- app/components/comstorage.tsx
+++ app/components/comstorage.tsx
@@ -6,39 +6,61 @@
 
 export default function Comstorage() {
     const comments = [{
+        id: 1,
         comment: "DRACHEN, DRACHEN,DRACHEN, ME DIZ OQUE TU SIENTE AO VER DRACHEN JUEGAR,DALE DALE DALE ÔÔÔÔÔ",
-        name: "João Victor",
+        name: "~ João Victor",
         data: "20/01/2023"
     },
     {
+        id: 2,
         comment: "Boa Vitoria,merecida!!",
         name: "Naruê Goncalves",
         data: "30/03/2023"
     },
     {
+        id: 3,
         comment: "Vai Drachen",
         name: "Lilian Semann",
         data: "14/04/2023"
     },
     {
-        comment: "Belo touchdown",
-        name: "Rodrigo Valim",
+        id: 4,
+        comment: "Belo touchdown! Vamos Drachen!",
+        name: "Rodrigo Zunino Valim",
         data: "23/06/2023"
     },
     {
+        id: 5,
         comment: "DIA DE GAME, VAMOS",
         name: "Patrick Dias",
         data: "07/08/2023"
     },
     {
+        id: 6,
         comment: "VOCÊS SÃO, RIDICULOS!!!!!",
         name: "João Krause",
         data: "1/10/2023"
+    },
+    {
+        id: 7,
+        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        name: "Larissa Butzke",
+        data: "7/10/2023"
     }
     ]
+
+    const conj = comments.filter((i) => i.id === 7)
     return (
-        <main>
-            {comments.map((i)=>(i.comment,i.name,i.data))}
+        <main className="p-10">
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className="flex w-[670px] flex-col text-2xl justify-center items-center gap-36">{conj.map((i) => (i.comment))}</div>
+            <div className="gap-7">
+                <br></br>
+                <div className="flex justify-end text-base">{conj.map((i) => (i.name))}</div>
+                <div className="flex justify-end text-[0.5em]">{conj.map((i) => (i.data))}</div>
+            </div>
         </main>
     )
 
