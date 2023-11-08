a1171276f7e50ee76766dd9aa85da50e1fcd31fb
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-11-08 16:32:29 -0300

    drachen

--- app/components/comanager.tsx
+++ app/components/comanager.tsx
@@ -1,20 +1,75 @@
 'use client'
 
 import { styled } from "styled-components"
-import Comstorage from "./comstorage"
-import { useState,useEffect } from "react"
-import {BsFillArrowRightSquareFill} from "react-icons/bs"
-import {BsFillArrowLeftSquareFill} from "react-icons/bs"
+import { useState, useEffect } from "react"
+import { BsFillArrowRightSquareFill } from "react-icons/bs"
+import { BsFillArrowLeftSquareFill } from "react-icons/bs"
 
 export default function Comanager() {
 
+    let comments = [{
+        id: 1,
+        comment: "DRACHEN, DRACHEN,DRACHEN, ME DIZ OQUE TU SIENTE AO VER DRACHEN JUEGAR,DALE DALE DALE ÔÔÔÔÔ",
+        name: "~ João Victor",
+        data: "20/01/2023"
+    },
+    {
+        id: 2,
+        comment: "Boa Vitoria,merecida!!",
+        name: "Naruê Goncalves",
+        data: "30/03/2023"
+    },
+    {
+        id: 3,
+        comment: "Vai Drachen",
+        name: "Lilian Semann",
+        data: "14/04/2023"
+    },
+    {
+        id: 4,
+        comment: "Belo touchdown! Vamos Drachen!",
+        name: "Rodrigo Zunino Valim",
+        data: "23/06/2023"
+    },
+    {
+        id: 5,
+        comment: "DIA DE GAME, VAMOS",
+        name: "Patrick Dias",
+        data: "07/08/2023"
+    },
+    {
+        id: 6,
+        comment: "VOCÊS SÃO, RIDICULOS!!!!!",
+        name: "João Krause",
+        data: "1/10/2023"
+    },
+    {
+        id: 7,
+        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        name: "Larissa Butzke",
+        data: "7/10/2023"
+    }
+    ]
+    const conj = comments.filter((i) => i.id === 6)
+
+    // const [previous, setPrevious] = useState(comments[0])
+    const [advence, setAdvence] = useState(comments[0])
+
     return (
         <Wrapper>
-            <main className="flex items-center justify-between w-[1500px] h-[300px] text-yellow-300 p-16">
-                <button className="text-4xl hover:scale-125 transition-all"><BsFillArrowLeftSquareFill/></button>
-                <Comstorage/>
-                <button className="text-4xl hover:scale-125 transition-all"><BsFillArrowRightSquareFill/></button>
-            </main>
+            <section className="flex items-center justify-between w-[1500px] h-[300px] text-yellow-300 p-16">
+                <button onClick={() => advence == comments[(comments.)]?setAdvence(comments.id = 0):setAdvence(comments[comments.indexOf(advence)-1])} className="text-4xl hover:scale-125 transition-all"><BsFillArrowLeftSquareFill /></button>
+                <main className="p-10">
+                    <div className="mt-20 mb-9 flex w-[670px] flex-col text-2xl justify-center items-center gap-36">{advence.comment}</div>
+                    <div className="gap-7">
+                        <div className="flex justify-end text-base">{advence.name}</div>
+                        <div className="flex justify-end text-[0.5em]">{advence.data}</div>
+                    </div>
+                </main>
+                <button onClick={() => advence == comments[comments.length -1]?setAdvence(comments[0]):setAdvence(comments[comments.indexOf(advence)+2])} className="text-4xl hover:scale-125 transition-all"><BsFillArrowRightSquareFill /></button>
+                <button onClick={() => console.log(comments[comments.indexOf(advence)+2])}>aqui</button>
+                <button onClick={() => console.log(advence)}>aqui</button>
+            </section>
         </Wrapper>
     )
 }
